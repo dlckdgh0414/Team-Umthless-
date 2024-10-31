74dfee0c20278e6c922ec1878e321ccbd4acba05
arke0909 <arke0909@gmail.com>
2024-10-31 14:15:00 +0900

    fix : Entity

--- Assets/Work/UM/01.Scripts/Enity/Entity.cs
+++ Assets/Work/UM/01.Scripts/Enity/Entity.cs
@@ -1,14 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public abstract class Entity : MonoBehaviour
 {
+    protected Dictionary<Type, IEntityComponent> _components;
+
     protected Rigidbody2D RigidCompo;
-    protected GroundCheck GroundCheckCompo;
 
     private void Awake()
     {
         RigidCompo = GetComponent<Rigidbody2D>();
-        GroundCheckCompo = GetComponentInChildren<GroundCheck>(); 
+
+        _components = new Dictionary<Type, IEntityComponent>();
+        GetComponentsInChildren<IEntityComponent>(true).ToList()
+            .ForEach(component => _components.Add(component.GetType(), component));
+
+        InitComponents();
+    }
+
+    private void InitComponents()
+    {
+        _components.Values.ToList().ForEach(component => component.Initialize(this));
     }
 
     [SerializeField] protected MovementDataSO _moveData;

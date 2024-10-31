515b104626e458cf02b92c3cd2dd03c613d16cf4
jiwon0612 <iololloli9976@gmail.com>
2024-10-31 15:25:16 +0900

    rename : notifyValue, fix Ihacking, feat : player

--- Assets/Work/Jiwon/01.Scirpts/Player.cs
+++ Assets/Work/Jiwon/01.Scirpts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Cinemachine;
 
@@ -5,16 +6,61 @@ public class Player : MonoBehaviour
 {
     [field : SerializeField] public InputReader InputComp { get; private set; }
 
+    private CinemachineVirtualCamera _virtualCamera;
+
+    [Header("HackingSetting")]
+    public float maxHackingCharge;
+    public NotifyValue<float> _hackingCharging;
+    private bool _isHacking;
     private Entity _currentEntity;
-    private CinemachineVirtualCamera _virtualCamera; 
+    private Entity _nextEntity;
 
     private void OnEnable()
     {
-        
+        InputComp.OnHackingChargingEvent += HandheldHacking;
     }
-        
+
+    private void Awake()
+    {
+        _hackingCharging = new NotifyValue<float>();
+        _virtualCamera = GetComponent<CinemachineVirtualCamera>();
+
+        _hackingCharging.OnValueChanged += HandleHackingChanged;
+    }
+
     private void OnDisable()
     {
-        
+        InputComp.OnHackingChargingEvent -= HandheldHacking;
+    }
+    
+    private void HandheldHacking(bool isHacking)
+    {
+        _isHacking = isHacking;
+        if (isHacking)
+        {
+            Vector2 point = UnityEngine.InputSystem.Mouse.current.position.ReadValue();
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(point);
+            
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector3.forward);
+            if (hit.collider.TryGetComponent(out Entity entity))
+                _nextEntity = entity;
+        }
+    }
+
+    private void Update()
+    {
+        if (_isHacking)
+        {
+            _hackingCharging.Value += Time.deltaTime;
+        }
+    }
+    
+    private void HandleHackingChanged(float prev, float next)
+    {
+        // if (next > maxHackingCharge)
+        // {
+        //     _currentEntity = _nextEntity;
+        //     _currentEntity.
+        // }
     }
 }

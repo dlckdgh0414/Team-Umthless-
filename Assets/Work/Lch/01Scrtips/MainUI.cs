43210b145e067af6a633e09fe8df07de15fca4bc
dlckdgh0414 <ci5976547@gmail.com>
2024-11-01 08:43:22 +0900

    faet : Text

--- Assets/Work/Lch/01Scrtips/MainUI.cs
+++ Assets/Work/Lch/01Scrtips/MainUI.cs
@@ -2,13 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class MainUI : MonoBehaviour
 {
 	[SerializeField] private Image _image;
+    [SerializeField] private TextMeshProUGUI _tmp;
+    [SerializeField] private TextListSO _list;
+    private int count = 0;
+    public bool isTextTrigger;
 
     private void Start()
     {
         _image.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (isTextTrigger)
+        {
+            _image.gameObject.SetActive(true);
+            DOText.DOTexting(_list._textList[count].Text, _tmp, 2,DG.Tweening.Ease.Linear, ()=>count++);
+        }
+        else if (!isTextTrigger)
+        {
+            _image.gameObject.SetActive(false);        }
     }
 }

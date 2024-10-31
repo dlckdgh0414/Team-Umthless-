896caa52e54cd44b76e068d1afaec6082db170c4
dlckdgh0414 <ci5976547@gmail.com>
2024-10-31 15:21:34 +0900

    feat : BrokenWall

--- Assets/Work/Lch/01Scrtips/EffectFeedback.cs
+++ Assets/Work/Lch/01Scrtips/EffectFeedback.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GGMPool;
 
 public class EffectFeedback : Feedback
 {
+    [SerializeField] private PoolManagerSO _poolManager;
+    [SerializeField] private PoolTypeSO _effectType;
+
     public override void PlayFeedback()
     {
-        
+        var effect = _poolManager.Pop(_effectType) as EffectPlay;
+        effect.EffectPlayer(transform.position, transform.rotation);
     }
 
     public override void StopFeedback()

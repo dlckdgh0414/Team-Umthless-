08de5d48f6dc4a2c4e9f23432978a1c983b6bf65
jiwon0612 <iololloli9976@gmail.com>
2024-10-31 19:11:34 +0900

    fix : playerHacking

--- Assets/Work/Jiwon/01.Scirpts/Entity/Lizard.cs
+++ Assets/Work/Jiwon/01.Scirpts/Entity/Lizard.cs
@@ -5,16 +5,26 @@ using System;
 
 public class Lizard : Entity
 {
-    
-    
+    private WallCheck _wallCheck;
+    protected override void Awake()
+    {
+        base.Awake();
+        _wallCheck = GetComponentInChildren<WallCheck>();
+    }
+
     public override void HackingEnter(Player player)
     {
         _player = player;
+        _player.InputComp.OnSkillEvent += HandleWallRunEvent;
+    }
+
+    private void HandleWallRunEvent()
+    {
         
     }
 
     public override void HackingExit()
     {
-        
+        _player.InputComp.OnSkillEvent -= HandleWallRunEvent;
     }
 }
--- Assets/Work/Jiwon/01.Scirpts/Entity/WallCheck.cs
+++ Assets/Work/Jiwon/01.Scirpts/Entity/WallCheck.cs
@@ -1,16 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System;
+using UnityEngine;
 
 public class WallCheck : MonoBehaviour
 {
     [Header("Setting")]
     [SerializeField] private Vector2 wallCheckSize;
-    private Transform wallCheckPostiton;
+    [SerializeField] private LayerMask whatIsWall;
 
-    // public bool IsWallCheck()
-    // {
-    //     
-    // }
+    public bool IsWallCheck()
+    {
+        bool isWall = Physics2D.OverlapBox(transform.position, wallCheckSize, 0, whatIsWall);
+        return isWall;
+    }
+
+#if UNITY_EDITOR
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, wallCheckSize);
+        Gizmos.color = Color.white;
+    }
+
+#endif
 }

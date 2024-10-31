3fe41911b5fd4d14192c3c43d01a6f5cce28137c
jiwon0612 <iololloli9976@gmail.com>
2024-11-01 08:15:52 +0900

    fix : lizard

--- Assets/Work/Jiwon/01.Scirpts/Entity/WallCheck.cs
+++ Assets/Work/Jiwon/01.Scirpts/Entity/WallCheck.cs
@@ -20,12 +20,12 @@ public class WallCheck : MonoBehaviour
     public bool IsWallCheck()
     {
         Collider2D wall = Physics2D.OverlapBox(transform.position, wallCheckSize, 0, whatIsWall);
-
+        
         if (wall == null) return false;
-
+        
         float dir = wall.transform.position.x - _entity.transform.position.x;
         isRightWall = Mathf.Sign(dir) > 0;
-
+        
         return wall != null;
     }
 

7ac4ad5d85a6635e1df2c91eb13d302e83375612
dlckdgh0414 <ci5976547@gmail.com>
2024-11-01 07:53:06 +0900

    feat : IconChange

--- Assets/Work/Lch/01Scrtips/lconChange.cs
+++ Assets/Work/Lch/01Scrtips/lconChange.cs
@@ -6,9 +6,16 @@ using UnityEngine.UI;
 public class lconChange : MonoBehaviour
 {
 	private Image _iconMask;
+    private Entity _player;
 
     private void Awake()
     {
+        _player = FindObjectOfType<Entity>();
         _iconMask = GetComponentInChildren<Image>();
+    }
+
+    private void Update()
+    {
+        _iconMask.sprite = _player._moveData.IconSprite;
     }
 }

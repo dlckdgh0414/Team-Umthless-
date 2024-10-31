8b2b7631d52a25761a395ae3743cfc980861e85e
arke0909 <arke0909@gmail.com>
2024-10-31 22:01:54 +0900

    feat : Bat

--- Assets/Work/UM/01.Scripts/Animals/Bat.cs
+++ Assets/Work/UM/01.Scripts/Animals/Bat.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bat : Entity
 {
+    [SerializeField] private float _flyPower;
     protected override void Awake()
     {
         base.Awake();
@@ -13,16 +15,29 @@ public class Bat : Entity
 
     public override void HackingEnter(Player player)
     {
+        _player = player;
+        _player.InputComp.OnJumpChargingEvent += Fly;
         _canMove = true;
     }
 
-    protected override void Move(Vector2 dir)
+    private void Fly(bool isFly)
     {
-        RigidCompo.velocity = dir * _moveData.moveSpeed;
+        RigidCompo.velocity = Vector2.zero;
+
+        if (isFly)
+        {
+            RigidCompo.AddForce(Vector2.up * _flyPower, ForceMode2D.Impulse);
+        }
+        else
+        {
+            RigidCompo.AddForce(Vector2.down * _flyPower * 2, ForceMode2D.Impulse);
+        }
     }
 
     public override void HackingExit()
     {
         _canMove = false;
+        _player.InputComp.OnJumpEvent -= Jump;
+        _player = null;
     }
 }

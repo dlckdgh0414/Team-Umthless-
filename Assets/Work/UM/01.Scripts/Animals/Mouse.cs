db9b02c4af6cc123748e5a7a699fd884a7847127
arke0909 <arke0909@gmail.com>
2024-10-31 14:36:12 +0900

    feat : MoveData SO

---
+++ Assets/Work/UM/01.Scripts/Animals/Mouse.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouse : Entity, IHackingEnter, IHackingExit
+{
+    public void HackingEnter()
+    {
+
+    }
+
+    public void HackingExit()
+    {
+    }
+}

ac530ebc3de851b1d4ca8942d1e1394f01cdef2e
arke0909 <arke0909@gmail.com>
2024-10-31 22:24:13 +0900

    feat : Rat Animation & fix : Bat

--- Assets/Work/UM/01.Scripts/Animals/Rat.cs
+++ Assets/Work/UM/01.Scripts/Animals/Rat.cs
@@ -1,9 +1,25 @@
+using UnityEngine;
+
 public class Rat : Entity
 {
+    [SerializeField] private AnimTypeSO _moveType;
     public override void HackingEnter(Player player)
     {
         _player = player;
         _canMove = true;
+    }
+
+    protected override void Move(Vector2 dir)
+    {
+        base.Move(dir);
+        if (dir.x != 0)
+        {
+            AnimCompo.SetParam(_moveType, true);
+        }
+        else
+        {
+            AnimCompo.SetParam(_moveType, false);
+        }
     }
 
     public override void HackingExit()

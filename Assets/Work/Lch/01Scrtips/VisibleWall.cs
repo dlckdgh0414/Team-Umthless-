07852dbb0b3a30d5076cdd4f1dc4968027d309ef
dlckdgh0414 <ci5976547@gmail.com>
2024-10-31 15:32:16 +0900

    feat : InvisibleWall

---
+++ Assets/Work/Lch/01Scrtips/VisibleWall.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using DG.Tweening;
+
+public class VisibleWall : MonoBehaviour
+{
+    private SpriteRenderer _sprite;
+
+    public UnityEvent OnVisibleWall;
+
+    private void Awake()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        _sprite.material.DOFade(0, 0.01F);
+    }
+
+    public void VisibleingWall()
+    {
+        _sprite.material.DOFade(1, 1.5f);
+    }
+}

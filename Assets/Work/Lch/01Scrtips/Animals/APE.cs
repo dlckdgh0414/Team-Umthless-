a39b8bcaeae1d747e17794e88791460d28277c68
dlckdgh0414 <ci5976547@gmail.com>
2024-11-01 01:12:10 +0900

    fix : InisibleWall

---
+++ Assets/Work/Lch/01Scrtips/Animals/APE.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class APE : Entity
+{
+    public override void HackingEnter(Player player)
+    {
+        
+    }
+
+    public override void HackingExit()
+    {
+        throw new System.NotImplementedException();
+    }
+}

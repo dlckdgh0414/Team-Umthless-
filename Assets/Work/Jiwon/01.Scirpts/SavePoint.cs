2a71b294a54c452df6462adc979ede9c8914cd7d
jiwon0612 <iololloli9976@gmail.com>
2024-11-01 09:27:12 +0900

    feat : savePoint

--- Assets/Work/Jiwon/01.Scirpts/SavePoint.cs
+++ Assets/Work/Jiwon/01.Scirpts/SavePoint.cs
@@ -5,5 +5,37 @@ using System;
 
 public class SavePoint : MonoBehaviour
 {
-    //[SerializeField] private List<>
+    public LayerMask whatIsTarget;
+    private List<Transform> savePoints;
+    private Transform _currentSavePoint;
+    private Transform _nextSavePoint;
+    private Entity _target;
+    public Player _player;
+
+    private void Awake()
+    {
+        foreach (Transform item in transform)
+        {
+            if (item.TryGetComponent(out Point point))
+            {
+                point.Initialized(this);
+                savePoints.Add(item);
+            }
+        }
+    }
+
+    public void ReSpawn()
+    {
+        _player.Hacking(_target);
+    }
+
+    public void SetTarget(Entity target)
+    {
+        _target = target;
+    }
+
+    public void SetSavePointe(Point point)
+    {
+        _currentSavePoint = point.transform;
+    }
 }
